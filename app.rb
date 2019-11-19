16e8a93899411d7836214904108edee768c3ffe7
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-11-19 17:10:55 +0000

    post/redirect/GET

--- app.rb
+++ app.rb
@@ -1,19 +1,29 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+  enable :sessions
   get '/' do
     erb :index
   end
 
 
-  post '/names' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
-    erb(:play)
-  end
+post '/names' do
+  session[:player1] = params[:player1]
+  session[:player2] = params[:player2]
+  redirect '/play'
 end
-#     'Welcome to the battle'
-#   end
+
+get '/play' do
+@player1 = session[:player1]
+@player2 = session[:player2]
+erb :play
+end
+
+run! if app_file == $0
+end
+
+#
 #
 #   # start the server if ruby file executed directly
   # run! if app_file == $0
