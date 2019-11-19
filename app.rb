ac9ac78c3204fd193a92d8cff3e36a3f8f4bce1a
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-11-19 16:37:26 +0000

    post names, play

--- app.rb
+++ app.rb
@@ -2,9 +2,19 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle!'
+    erb :index
   end
 
-  # start the server if ruby file executed directly
-  run! if app_file == $0
+
+  post '/names' do
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb(:play)
+  end
 end
+#     'Welcome to the battle'
+#   end
+#
+#   # start the server if ruby file executed directly
+  # run! if app_file == $0
+# end
