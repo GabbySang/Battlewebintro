f76f5ff257fc72fb56d24c47a89d3f0dfef8d2bd
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-11-19 14:52:35 +0000

    initialize

---
+++ spec/features/player_names_spec.rb
@@ -0,0 +1,10 @@
+feature 'Enter and display player names' do
+  scenario 'Each player enters their name' do
+    visit '/'
+    within(:css, '#player_names_form') do
+      fill_in :player1, with: 'Rahul'
+      fill_in :player2, with: 'Gabby'
+      click_button 'Play!'
+    end
+  end
+end
